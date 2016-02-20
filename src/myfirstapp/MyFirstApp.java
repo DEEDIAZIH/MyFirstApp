6e89d549bdaa78b1598f053bd099a6b4857e77eb
Chiamaka Wendy Azih <cazih@claflin.edu>
2016-02-20 14:04:52 -0500

    Added files via upload

---
+++ src/myfirstapp/MyFirstApp.java
@@ -0,0 +1,64 @@
+
+package myfirstapp;
+import java.util.Scanner;
+
+public class MyFirstApp {
+
+  
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        
+    System.out.println("     **************");
+    System.out.println("    *            **");
+    System.out.println("   *            * *");
+    System.out.println("  *            *  *");
+    System.out.println(" *            *   *");
+    System.out.println("**************    *");
+    System.out.println("*            *    *");
+    System.out.println("*            *    *");
+    System.out.println("*            *    *");
+    System.out.println("*            *   *");
+    System.out.println("*            *  *");
+    System.out.println("*            * *");
+    System.out.println("*            **");
+    System.out.println("**************");
+  
+      }
+
+}  
+
+
+  
+
+
+
+ 
+
+
+  
+
+
+    
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
